750ee7775ca4d27c93d72f0ffc88671094b8b9b4
lisps <stummp@loewen.de>
2014-02-05 09:42:03 +0100

    fix test to work with old-stable

--- _test/helper.test.php
+++ _test/helper.test.php
@@ -14,16 +14,16 @@ class plugin_ajax_helper_test extends DokuWikiTest {
 
     public function test_get_wikiFile() {
         global $INFO;
-        $_POST['pageid'] = 'mailinglist';
-        $_POST['lastmod'] = @filemtime(wikiFn('mailinglist'));
+        $_POST['pageid'] = 'wiki:dokuwiki';
+        $_POST['lastmod'] = @filemtime(wikiFn('wiki:dokuwiki'));
         $wiki = $this->h->getWikiPage();
-        $this->assertContains('Netiquette',$wiki);
+        $this->assertContains('DokuWiki',$wiki);
     }
     
     public function test_get_wikiFile_oldlastmod() {
         global $INFO;
-        $_POST['pageid'] = 'mailinglist';
-        $_POST['lastmod'] = @filemtime(wikiFn('mailinglist')) -1;
+        $_POST['pageid'] = 'wiki:dokuwiki';
+        $_POST['lastmod'] = @filemtime(wikiFn('wiki:dokuwiki')) -1;
 
         ob_start();
         $this->h->getWikiPage();
